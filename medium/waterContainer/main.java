71031a043e5372713d99508771ed5f69b7cbda21
Lemon <leedhakal85@gmail.com>
2024-06-02 16:17:49 -0400

    final solution

--- medium/waterContainer/main.java
+++ medium/waterContainer/main.java
@@ -5,14 +5,20 @@ public class main {
         int maxArea = 0,left =0, right = height.length-1;
 
         while(left<right){
-
+            int localArea = Math.abs(left-right)*Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, localArea);
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
         }
-        return 0;
+        return maxArea;
     }
     public static void main(String[] args){
 
        int height[] = {1,8,6,2,5,4,8,3,7};
-       solution(height);
+      System.out.print( solution(height));
 
     }
 }
